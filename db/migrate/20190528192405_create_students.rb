8daa22f419f8d1fce3512ce97b428d50ac7c2457
Walter Pacheco <walter.e.pacheco.chandia@gmail.com>
2019-05-28 16:24:55 -0400

    Se agrega estudiante por consola

--- db/migrate/20190528192405_create_students.rb
+++ db/migrate/20190528192405_create_students.rb
@@ -3,6 +3,7 @@ class CreateStudents < ActiveRecord::Migration[5.2]
     create_table :students do |t|
       t.string :name
       t.integer :age
+      t.string :email
 
       t.timestamps
     end
